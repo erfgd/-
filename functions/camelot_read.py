0e98c9c6f8ab908cc383323d9a64106a924e0272
apdoronkina <100873734+apdoronkina@users.noreply.github.com>
2022-04-09 16:25:06 +0300

    Update camelot_read.py

--- functions/camelot_read.py
+++ functions/camelot_read.py
@@ -1,5 +1,15 @@
 import camelot
-
-#для ани доронкиной
+# Для Анечки
 def camelot_func(filename):
-    pass
+    tables = camelot.read_pdf(filename, pages = '1, 2-end', process_background = True)
+    tables
+    tables.export('filename.csv', f = 'csv', compress = True)
+    i=0
+    while i <= 9:
+        tables[i]
+        tables[i].parsing_report
+        tables[i].to_csv('filename.csv')
+        tables[i].df
+        i = i + 1
+        
+# Роман, если что, помни, пожалуйста, что я глупая        
